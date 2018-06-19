0b7ad42d01b0a47d08c7bc1b056dbc582f683200
soonws <soonws97@gmail.com>
2018-06-19 15:43:57 +0800

    main page change

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -52,7 +52,7 @@ $this->title = 'Jio Me out';
 <div id="services" class="text-center">
   <div class="container">
     <div class="section-title center">
-      <h2>JOIN OUR COMMUNITY</h2>
+      <h2>OUR FEATURES</h2>
       <hr>
     </div>
     <div class="space"></div>
@@ -115,111 +115,33 @@ $this->title = 'Jio Me out';
 <div id="works">
   <div class="container"> <!-- Container -->
     <div class="section-title text-center center">
-      <h2>Our Portfolio</h2>
+      <h2>easy login</h2>
       <hr>
       <div class="clearfix"></div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
     </div>
     <div class="categories">
-      <ul class="cat">
-        <li>
-          <ol class="type col-lg-12">
-            <li><a href="#" data-filter="*" class="active">All</a></li>
-            <li><a href="#" data-filter=".lorem">Web Design</a></li>
-            <li><a href="#" data-filter=".consectetur">Web Development</a></li>
-            <li><a href="#" data-filter=".dapibus">Branding</a></li>
-          </ol>
-        </li>
-      </ul>
-      <div class="clearfix"></div>
+      <div><a href="#google" class="btn btn-google"><i class="fa fa-google"></i>Login with Google</a></div>
+      <p>Don’t want to commit yet? Explore our features as guest user with no string attached!</p>
+      <div><a href="#google" class="btn btn-tryit">Try it Out</a></div>
     </div>
-    <div class="row">
-      <div class="portfolio-items">
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/01.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Design</p>
-              </div>
-              <img src="img/portfolio/01.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+  </div>
+</div>
+<!-- Join Our Community -->
+<div id="join">
+  <div class="join-body">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1 phone-up">
+          <h1>JOIN OUR Community</h1>
+          <hr>
+          <p class='content'>Engage with other users, share anything to your friends, or report product issue to us. </p>
+          <p class='content'>Join us and create a better JIO-munity!</p>
           </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/02.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Development</p>
-              </div>
-              <img src="img/portfolio/02.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/03.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Design</p>
-              </div>
-              <img src="img/portfolio/03.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/04.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Design</p>
-              </div>
-              <img src="img/portfolio/04.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/05.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Development</p>
-              </div>
-              <img src="img/portfolio/05.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/06.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Branding</p>
-              </div>
-              <img src="img/portfolio/06.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus consectetur">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/07.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Development, Branding</p>
-              </div>
-              <img src="img/portfolio/07.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
-          <div class="portfolio-item">
-            <div class="hover-bg"> <a href="img/portfolio/08.jpg" title="Project description" rel="prettyPhoto">
-              <div class="hover-text">
-                <h4>Project Title</h4>
-                <p>Web Design</p>
-              </div>
-              <img src="img/portfolio/08.jpg" class="img-responsive" alt="Project Title"> </a> </div>
-          </div>
-        </div>
+          <div class="col-md-10 col-md-offset-1" style="text-align: center;"><a href="#about" class="btn btn-default page-scroll" style="background-color: #008080;">Take A Look</a> </div>
       </div>
     </div>
   </div>
 </div>
-
 <!-- Contact Section -->
 <div id="contact" class="text-center">
   <div class="container">
--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -98,7 +98,8 @@ function main() {
   function initParallax() {
     $('#intro').parallax("100%", 0.3);
     $('#services').parallax("100%", 0.3);
-    $('#aboutimg').parallax("100%", 0.3);	
+    $('#aboutimg').parallax("100%", 0.3);
+    $('#join').parallax("100%", 0.3);	
     $('#testimonials').parallax("100%", 0.1);
 
   }
